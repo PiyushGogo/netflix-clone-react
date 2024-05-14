24c36c867dbad5421f971e53fda4b16a0d6549e0
Piyush cn <piyush.gogoriya@capitalnow.in>
2024-05-14 20:56:24 +0530

    more cards added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,45 @@
 import React from "react";
 import "./Home.css";
 import Navbar from "../../components/Navbar/Navbar";
+import HeroBanner from "../../assets/hero_banner.jpg";
+import HeroTitle from "../../assets/hero_title.png";
+import PlayIcon from "../../assets/play_icon.png";
+import InfoIcon from "../../assets/info_icon.png";
+import TitileCards from "../../components/TitileCards/TitleCards";
 
 const Home = () => {
   return (
     <div className="home">
       <Navbar />
+      <div className="hero">
+        <img src={HeroBanner} alt="" className="banner-img" />
+        <div className="hero-caption">
+          <img src={HeroTitle} alt="" className="caption-img" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo,
+            quasi? Nobis, possimus sequi, perspiciatis earum eum ea cupiditate,
+            nemo totam incidunt similique culpa voluptatum reprehenderit
+            expedita porro fuga! Aspernatur, laboriosam.
+          </p>
+          <div className="hero-btn">
+            <button className="btn">
+              <img src={PlayIcon} alt="" />
+              Play
+            </button>
+            <button className="btn dark-btn">
+              <img src={InfoIcon} alt="" />
+              More Info
+            </button>
+          </div>
+          <TitileCards />
+        </div>
+      </div>
+      <div className="more-cards">
+        <TitileCards />
+        <TitileCards />
+        <TitileCards />
+        <TitileCards />
+      </div>
     </div>
   );
 };
