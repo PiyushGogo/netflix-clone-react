6e445254c77078d8622f1b4ec47412917e1129be
Piyush cn <piyush.gogoriya@capitalnow.in>
2024-05-14 13:57:48 +0530

    (page): Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,39 @@
 import React from "react";
 import "./Navbar.css";
+import logo from "../../assets/logo.png";
+import searchIcon from "../../assets/search_icon.svg";
+import bellIcon from "../../assets/bell_icon.svg";
+import profileImg from "../../assets/profile_img.png";
+import caretIcon from "../../assets/caret_icon.svg";
 
 const Navbar = () => {
-  return <div className="navbar"></div>;
+  return (
+    <div className="navbar">
+      <div className="bnavbar-left">
+        <img src={logo} alt="" />
+        <ul>
+          <li>Home</li>
+          <li>TV Shows</li>
+          <li>Movies</li>
+          <li>New & Popular</li>
+          <li>My List</li>
+          <li>Browse By Languages</li>
+        </ul>
+      </div>
+      <div className="navbar-right">
+        <img src={searchIcon} className="icons" alt="" />
+        <p>Childern</p>
+        <img src={bellIcon} className="icons" alt="" />
+        <div className="navbar-profile">
+          <img src={profileImg} className="profile" alt="" />
+          <img src={caretIcon} alt="" />
+          <div className="dropdown">
+            <p>Sign out of Netflix</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
